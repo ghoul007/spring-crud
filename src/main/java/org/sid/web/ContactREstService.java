c1474247d50918d3bf9e6479cad8683c96ed7dd9
Travis CI <ghoul.ahmed5@gmail.com>
2019-12-14 09:37:22 +0100

    add cross alow annotation

--- src/main/java/org/sid/web/ContactREstService.java
+++ src/main/java/org/sid/web/ContactREstService.java
@@ -7,6 +7,7 @@ import org.sid.entities.Contact;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class ContactREstService {
 	
 	@Autowired
